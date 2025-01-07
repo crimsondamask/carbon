12c7d47d675b74a8a4a12415828111576fd3607d
Abdelkader Madoui <abdelkadermadoui@gmail.com>
2025-01-07 13:27:50 +0100

    Working on component struct.

--- src/components.rs
+++ src/components.rs
@@ -1,10 +1,10 @@
+use crate::mutex_data::MutexData;
 use egui::{
     style::Selection, Button, Color32, ComboBox, Label, PointerButton, RichText, Rounding, Slider,
     Stroke, Ui, Vec2, Visuals,
 };
 use egui_phosphor;
 use epaint::Pos2;
-use crate::mutex_data::MutexData;
 pub enum Component {
     AnalogSensor(AnalogSensorConfig),
     DigitalSensor,
@@ -14,8 +14,10 @@ pub enum Component {
 
 /// Analog sensor widget that displays the value of a sensor with a tag.
 pub struct AnalogSensorConfig {
+    pub id: usize,
     pub tag: String,
     pub value: f32,
+    pub unit: String,
     pub setpoint_hh: f32,
     pub setpoint_ll: f32,
     pub alarm_hh: bool,
@@ -23,18 +25,52 @@ pub struct AnalogSensorConfig {
     pub pos: Pos2,
 }
 
-
-pub fn render_component(ui: &mut Ui, component: Component) {
+pub fn render_component(ui: &mut Ui, component: &mut Component, edit: bool) {
     match component {
-        Component::AnalogSensor(config) => {}
+        Component::AnalogSensor(config) => {
+            let tag = ui.put(
+                egui::Rect {
+                    min: Pos2::new(config.pos.x, config.pos.y - 40.),
+                    max: Pos2::new(config.pos.x + 150., config.pos.y + 30.),
+                },
+                Label::new(
+                    RichText::new(format!("   {}   ", config.tag))
+                        .size(14.)
+                        .strong()
+                        .color(Color32::BLACK)
+                        .background_color(Color32::GRAY),
+                )
+                .sense(egui::Sense {
+                    click: true,
+                    drag: true,
+                    focusable: true,
+                }),
+            );
+            let widget = ui.put(
+                egui::Rect {
+                    min: config.pos,
+                    max: Pos2::new(config.pos.x + 150., config.pos.y + 30.),
+                },
+                Label::new(
+                    RichText::new(format!("  {:.02}  {}   ", config.value, config.unit))
+                        .size(14.)
+                        .strong()
+                        .color(Color32::WHITE)
+                        .background_color(Color32::BLACK),
+                )
+                .sense(egui::Sense {
+                    click: true,
+                    drag: true,
+                    focusable: true,
+                }),
+            );
+        }
         Component::DigitalSensor => {}
         Component::Button => {}
         Component::ToggleSwitch => {}
     }
 }
 
-pub fn update_component(component: Component, mutex_data: MutexData) {
-
-}
+pub fn update_component(component: Component, mutex_data: MutexData) {}
 
 pub fn edit_component(component: Component) {}
